841c7a45ad7f4d20d56626a7cd2e2403a447145c
Koffi Cobbin <52169278+Koffi-Cobbin@users.noreply.github.com>
2021-08-01 15:07:11 +0000

    Update app.py

--- src/app.py
+++ src/app.py
@@ -25,49 +25,50 @@ def create_app(*args, **kwargs):
     except KeyError as e:
         env = 'development'
         print('Error:', e, 'Environment was not specified, defaulting to development')
+    return env
 
-    # create and configure the app
-    app = Flask(__name__, instance_relative_config=True)
+# create and configure the app
+app = Flask(__name__, instance_relative_config=True)
 
-    app.config['ENV'] = env
-    app.config.from_object('src.config.%s' % env)
+app.config['ENV'] = create_app(env='development')
+app.config.from_object('src.config.%s' % env)
 
-    app.config['SQLALCHEMY_DATABASE_URI'] = app.config['DATABASE_URI']
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = app.config['DATABASE_URI']
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-    # ensure the instance folder exists
-    try:
-        os.makedirs(app.instance_path)
-    except OSError:
-        pass
+# ensure the instance folder exists
+try:
+    os.makedirs(app.instance_path)
+except OSError:
+    pass
 
-    # Initialize CORS
-    CORS(app)
+# Initialize CORS
+CORS(app)
 
-    # Initialize JWT
-    jwt.init_app(app)
+# Initialize JWT
+jwt.init_app(app)
 
-    # Initialize database
-    db.init_app(app)
-    migrate.init_app(app, db, render_as_batch=True)
+# Initialize database
+db.init_app(app)
+migrate.init_app(app, db, render_as_batch=True)
 
-    @app.route('/')
-    def hello():
-        return "Hello from root of app /"
+@app.route('/')
+def hello():
+    return "Hello from root of app /"
 
-    # Register Blueprints
-    app.register_blueprint(auth_bp, url_prefix="/auth")
-    app.register_blueprint(user_bp, url_prefix="/users")
-    app.register_blueprint(document_bp, url_prefix="/documents")
-    app.register_blueprint(department_bp, url_prefix="/deparments")
-    app.register_blueprint(comment_bp, url_prefix="/comments")
-    app.register_blueprint(college_bp, url_prefix="/colleges")
-    app.register_blueprint(faculty_bp, url_prefix="/faculties")
-    app.register_blueprint(portfolio_bp, url_prefix="/portfolios")
-    app.register_blueprint(approval_bp, url_prefix="/approvals")
+# Register Blueprints
+app.register_blueprint(auth_bp, url_prefix="/auth")
+app.register_blueprint(user_bp, url_prefix="/users")
+app.register_blueprint(document_bp, url_prefix="/documents")
+app.register_blueprint(department_bp, url_prefix="/deparments")
+app.register_blueprint(comment_bp, url_prefix="/comments")
+app.register_blueprint(college_bp, url_prefix="/colleges")
+app.register_blueprint(faculty_bp, url_prefix="/faculties")
+app.register_blueprint(portfolio_bp, url_prefix="/portfolios")
+app.register_blueprint(approval_bp, url_prefix="/approvals")
 
-    with app.app_context():
-        # Reset Database
-        db.drop_all()   # Comment out if you want to use flask_migrate
-        db.create_all()  # Comment out if you want to use flask_migrate
-    return app
+with app.app_context():
+    # Reset Database
+    db.drop_all()   # Comment out if you want to use flask_migrate
+    db.create_all()  # Comment out if you want to use flask_migrate
+
