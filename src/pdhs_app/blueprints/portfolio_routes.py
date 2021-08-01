02561b2fa7b1b5c8fa19b0f92df6951c56c43f61
Koffi Cobbin <52169278+Koffi-Cobbin@users.noreply.github.com>
2021-08-01 13:33:48 +0000

    Update portfolio_routes.py

--- src/pdhs_app/blueprints/portfolio_routes.py
+++ src/pdhs_app/blueprints/portfolio_routes.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, request, jsonify
-from pdhs_app.models.users.user import User  # src.
-from pdhs_app.models.portfolios.portfolio import Portfolio
+from src.pdhs_app.models.users.user import User  # src.
+from src.pdhs_app.models.portfolios.portfolio import Portfolio
 
 bp = Blueprint('portfolios', __name__, url_prefix='/portfolio')
 
