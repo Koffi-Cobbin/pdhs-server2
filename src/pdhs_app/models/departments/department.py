beb2ab7f1d1d9cce9908409d953bbbe9039d4305
Koffi Cobbin <52169278+Koffi-Cobbin@users.noreply.github.com>
2021-08-03 23:18:05 +0000

    Update department.py

--- src/pdhs_app/models/departments/department.py
+++ src/pdhs_app/models/departments/department.py
@@ -6,7 +6,6 @@ class Department(db.Model):
     name = db.Column(db.String(100), nullable=False)
     faculty_id = db.Column(db.Integer, db.ForeignKey('faculty.id'), nullable=False)
     users = db.relationship("User", lazy='select', backref=db.backref('department', lazy='joined'))
-#     head = db.Column(db.Integer, db.ForeignKey('user.id', use_alter=True), nullable=True)
 
     def __repr__(self):
         return '<Department %r>' % self.name
