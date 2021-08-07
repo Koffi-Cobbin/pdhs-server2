feff0893f5fb771c34e2b334cfbc6b810f5e59a3
Koffi Cobbin <52169278+Koffi-Cobbin@users.noreply.github.com>
2021-08-07 20:10:02 +0000

    Update user_routes.py

--- src/pdhs_app/blueprints/user_routes.py
+++ src/pdhs_app/blueprints/user_routes.py
@@ -67,7 +67,8 @@ def get_all_users():
             return jsonify({'msg': 'Ther are no registered users'}), 404
         return jsonify({'users': users})
 
-    @bp.route('delete/<int:user_id>', methods=['POST'])
+
+@bp.route('delete/<int:user_id>', methods=['DELETE'])
 def delete_user(user_id):
     if request.method == 'DELETE':
         user = User.find_by_id(user_id)
