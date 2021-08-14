d92fcb33e1ea420df9b70e5226d6b69eadf82b50
Koffi Cobbin <52169278+Koffi-Cobbin@users.noreply.github.com>
2021-08-14 16:51:49 +0000

    Update document_routes.py

--- src/pdhs_app/blueprints/document_routes.py
+++ src/pdhs_app/blueprints/document_routes.py
@@ -193,7 +193,11 @@ def get_user_documents(user_id):
                 recipient_portfolio = Portfolio.find_by_id(recipient.portfolio_id) 
                 recipients.append(recipient_portfolio.name)
                 statuses.append(approval.status)
-            document.approval_list = dict(zip(recipients.reverse(), statuses.reverse()))
+                if recipients:
+                    recipients.reverse()
+                if statuses:
+                    statuses.reverse()
+            document.approval_list = dict(zip(recipients, statuses))
             user_documents.append(document.to_json())
         print("=========================Sent Documents===============================", user_documents)
         return jsonify(documents=user_documents)
