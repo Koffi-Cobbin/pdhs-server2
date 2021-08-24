dc9a1c43cd1114c8b082d35488ae859880c7f337
Koffi Cobbin <52169278+Koffi-Cobbin@users.noreply.github.com>
2021-08-24 15:33:43 +0000

    Update cloud_upload.py

--- src/middleware/cloud_upload.py
+++ src/middleware/cloud_upload.py
@@ -5,9 +5,9 @@ from cloudinary.utils import cloudinary_url
 
 
 cloudinary.config(
-    cloud_name = "thehivecloudstorage",
-    api_key = "159559222499375",
-    api_secret = "Y63wxrblUk5qU5ls77ln6O_Gxec"
+    cloud_name =  environ.get('CLOUD_NAME')   #"thehivecloudstorage",
+    api_key = environ.get('API_KEY')   #"159559222499375",
+    api_secret = environ.get('API_SECRET')    #"Y63wxrblUk5qU5ls77ln6O_Gxec"
 )
 
 def upload_file(file_to_upload):
