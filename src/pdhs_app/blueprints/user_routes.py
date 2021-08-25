3939c752841def5a0a68828a2d89028ba152e0a5
Koffi Cobbin <52169278+Koffi-Cobbin@users.noreply.github.com>
2021-08-25 20:20:35 +0000

    Update user_routes.py

--- src/pdhs_app/blueprints/user_routes.py
+++ src/pdhs_app/blueprints/user_routes.py
@@ -51,9 +51,10 @@ def update_user(user_id):
     user = User.find_by_id(user_id)
     
     approvals = Approval.query.filter_by(recipient_id=user_id)
-    for approval in approvals:
-        if approval:
-            approval.delete_from_db()
+    if approvals:
+        for approval in approvals:
+            if approval:
+                approval.delete_from_db()
             
     docs = Document.find_by_user_id(user_id)
     for doc in docs:
