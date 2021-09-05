b01243a8621fc2eeee6536d853afb33b4e4675c6
Koffi Cobbin <52169278+Koffi-Cobbin@users.noreply.github.com>
2021-09-05 12:00:27 +0000

    Update document_routes.py

--- src/pdhs_app/blueprints/document_routes.py
+++ src/pdhs_app/blueprints/document_routes.py
@@ -207,7 +207,7 @@ def get_user_documents(user_id):
             for approval in doc_approvals:
                 recipient = User.find_by_id(approval.recipient_id)
                 recipient_portfolio = Portfolio.find_by_id(recipient.portfolio_id) 
-                Print("===============================>>>>", recipient_portfolio.name, recipient.department_id)
+                print("===============================>>>>", recipient_portfolio.name, recipient.department_id)
                 recipients.append(recipient_portfolio.name + " (" + recipient.department_id + ")")
                 statuses.append(approval.status)
             document.approval_list = dict(zip(recipients, statuses))
